429ccc3fcc34cc473e01ba03394a879f61a86c36
PsychCodez <116991374+PsychCodez@users.noreply.github.com>
2025-03-13 23:12:22 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "PES2UG22CS193 Here\n";
+  cout << "CCLAB4\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
