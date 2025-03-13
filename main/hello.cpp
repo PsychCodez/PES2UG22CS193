dfdb406fc916919ca0734c313432f3763c7b4df4
PsychCodez <116991374+PsychCodez@users.noreply.github.com>
2025-03-13 22:34:15 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Worlds\n";
+  cout << "Hello, Jenkins CCLAB4\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
